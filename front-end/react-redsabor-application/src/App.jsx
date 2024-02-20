b2c9c3e5c4cbbfcd58e53fbf9196734d5b4163d1
Leo Oviedo <leoxpro050@gmail.com>
2024-02-20 02:22:11 -0300

    Agregando cambiosde react router dom a App.jsx

--- front-end/react-redsabor-application/src/App.jsx
+++ front-end/react-redsabor-application/src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import Register from './components/Register'
+import Login from './components/Login'
 import './App.css'
 
 function App() {
 
-  return <Register/>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element= {<Register/>}/>
+        <Route path='/login' element= {<Login/>}/>
+        <Route path='/' element= {<h1>Home</h1>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
